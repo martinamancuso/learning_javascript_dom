b32de13802aee7a53270fafe4e0bab710ca4d1ef
martinamancuso <martinamancuso24@gmail.com>
2024-01-26 12:14:30 +0100

    manipolazione del dom exercise

---
+++ capitolo_1/manipolazione_del_dom/index.js
@@ -0,0 +1,13 @@
+const addProduct = () => {
+  const ul = document.querySelector("ul");
+  const li = document.createElement("li");
+  const input = document.querySelector("input");
+  li.innerText = input.value;
+
+  const checkBox = document.createElement("input");
+  checkBox.setAttributeNode("type", "checkbox");
+  ul.appendChile(li);
+  li.appendChild(checkBox);
+};
+
+/* Esercizio svolto in pair con Paolo Lupo */
